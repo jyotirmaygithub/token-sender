9bc879cb3b609edaf7e27f8f3430f04ee371245c
jyotirmaygithub <jyotirmay2000gupta@gmail.com>
2024-08-21 16:33:48 +0530

    update : deploy.sh , lib.rs for the  getting token and token name funciton .

--- src/token-sender-frontend/src/components/Balance.jsx
+++ src/token-sender-frontend/src/components/Balance.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Principal } from "@dfinity/principal";
+import { token_sender_backend } from "../../../declarations/token-sender-backend";
 
 function Balance() {
-  
-  async function handleClick() {
-    console.log("Balance Button Clicked");
-  }
+  const [principalId, setPrincipalId] = useState("");
+  const [userBalance, setUserBalance] = useState(null);
+  const [tokenSymbol, setTokenSymbol] = useState(null);
 
+  async function handleClick() {
+    try {
+      const principal = Principal.fromText(principalId);
+
+      const balance = await token_sender_backend.get_user_balance(principal);
+      const symbol = await token_sender_backend.get_token_name();
+
+      // setUserBalance(balance);
+      setTokenSymbol(symbol);
+      setUserBalance(balance.toLocaleString())
+
+    } catch (error) {
+      console.error("Error fetching balance or token symbol:", error);
+    }
+  }
 
   return (
     <div className="window white">
@@ -15,6 +31,8 @@ function Balance() {
           id="balance-principal-id"
           type="text"
           placeholder="Enter a Principal ID"
+          value={principalId}
+          onChange={(e) => setPrincipalId(e.target.value)}
         />
       </p>
       <p className="trade-buttons">
@@ -25,7 +43,9 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of XYZ.</p>
+      <p>
+        This account has a balance of {tokenSymbol ? tokenSymbol : 'Token symbol'} - {userBalance !== null ? userBalance : 'Balance not available'}.
+      </p>
     </div>
   );
 }
